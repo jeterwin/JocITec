e6be94d099221449638e3ad3922988588813f866
mariogroza <mario.groza@proton.me>
2026-03-28 00:00:31 +0200

    Camera

---
+++ Assets/Scripts/CameraPan.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+
+    public GameObject virtualCam;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            virtualCam.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
+        {
+            virtualCam.SetActive(false);
+        }
+    }
+}
